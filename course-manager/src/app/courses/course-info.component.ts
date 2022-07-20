f123690886b6e055af67edbb399251d8fc9ef578
AnaniasMv <ananiasmatheus22@outlook.com>
2022-07-19 23:50:09 -0300

    variaveis de template

--- course-manager/src/app/courses/course-info.component.ts
+++ course-manager/src/app/courses/course-info.component.ts
@@ -6,12 +6,13 @@ import { CourseService } from "./course.service";
 @Component({
     templateUrl: './course-info.component.html'
 })
+
 export class CourseInfoComponent implements OnInit {
     course: Course | undefined;
 
     constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.course = this.courseService.retrieveById( (Number(this.activatedRoute.snapshot.paramMap.get('id'))));
+        this.course = this.courseService.retrieveById((Number(this.activatedRoute.snapshot.paramMap.get('id')))); 
     }
 }
